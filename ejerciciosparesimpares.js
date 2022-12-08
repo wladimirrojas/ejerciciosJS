e15eab778f151c5ceef393a7d14f7ae174693fb0
wladimirrojas <rajimiru@gmail.com>
2022-12-08 00:08:05 -0300

    ejercicios para virita

---
+++ ejerciciosparesimpares.js
@@ -0,0 +1,30 @@
+for(let i = 0; i < 10; i += 2){
+    console.log(i)
+}
+
+for(let i = -10; i < 10; i++){
+
+    if(i % 2 == 0){
+        console.log(i)
+    }
+}
+
+for(let i = -10; i < 10; i++){
+
+    if (i % 2 != 0){
+        console.log(i)
+    }
+}
+
+for(let i = 0; i < 20; i+=3){
+    console.log(i)
+}
+
+for(let i = 0; i <100; i+=3){
+
+    if(i % 2 != 0){
+        console.log(i)
+
+    }
+        console.log(i)
+}
---
+++ secuencianumericaunoenuno.js
@@ -0,0 +1,8 @@
+for(let i = 1; i <= 10 ; i++){
+    console.log(i)
+}
+for(let i = -100; i <= 455 ; i++){
+    if(i >= 1 && i <= 10){
+        console.log(i)
+    }
+}
